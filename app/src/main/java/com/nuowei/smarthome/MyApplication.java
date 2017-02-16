bc7ec1d8a8919a145c8d3d89f84d72ed90855264
xiaoli <554674787@qq.com>
2017-02-16 22:31:53 +0800

    2

--- app/src/main/java/com/nuowei/smarthome/MyApplication.java
+++ app/src/main/java/com/nuowei/smarthome/MyApplication.java
@@ -4,6 +4,11 @@ package com.nuowei.smarthome;/**
 
 import android.app.Application;
 
+import com.avos.avoscloud.AVOSCloud;
+import com.jiongbull.jlog.Logger;
+import com.jiongbull.jlog.constant.LogLevel;
+import com.jiongbull.jlog.constant.LogSegment;
+import com.jiongbull.jlog.util.TimeUtils;
 import com.squareup.leakcanary.LeakCanary;
 import com.taobao.hotfix.HotFixManager;
 import com.taobao.hotfix.PatchLoadStatusListener;
@@ -11,6 +16,9 @@ import com.taobao.hotfix.util.PatchStatusCode;
 import com.umeng.analytics.MobclickAgent;
 
 import org.litepal.LitePal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import im.fir.sdk.FIR;
 
@@ -23,6 +31,10 @@ import im.fir.sdk.FIR;
  */
 public class MyApplication extends Application {
 
+
+    private static MyApplication myApplication;
+    private static Logger sLogger;
+
     @Override
     public void onCreate() {
 
@@ -34,6 +46,9 @@ public class MyApplication extends Application {
      * 初始化sdk
      */
     private void initSDK() {
+
+        myApplication = this;
+
         //以下是内存泄露检查代码
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
@@ -75,5 +90,36 @@ public class MyApplication extends Application {
                     }
                 }).initialize();
 
+
+        List<String> logLevels = new ArrayList<>();
+        logLevels.add(LogLevel.ERROR);
+        logLevels.add(LogLevel.WTF);
+        //log打印
+        sLogger = Logger.Builder.newBuilder(getApplicationContext(), "jlog")
+                /* 下面的属性都是默认值，你可以根据需求决定是否修改它们. */
+                .setDebug(true)
+                .setWriteToFile(false)
+                .setLogDir("jlog")
+                .setLogPrefix("")
+                .setLogSegment(LogSegment.TWELVE_HOURS)
+                .setLogLevelsForFile(logLevels)
+                .setZoneOffset(TimeUtils.ZoneOffset.P0800)
+                .setTimeFormat("yyyy-MM-dd HH:mm:ss")
+                .setPackagedLevel(0)
+                .setStorage(null)
+                .build();
+        //意见反馈模组
+        AVOSCloud.initialize(this,"PUfaNAwuo1oW0qx7MRbNPVh8-gzGzoHsz","1zVJl0bgigUQIUl2wVPleRDI");
+        AVOSCloud.setDebugLogEnabled(true);
+
     }
+
+    public static Logger getLogger() {
+        return sLogger;
+    }
+
+    public static MyApplication getApp() {
+        return myApplication;
+    }
+
 }
